fec4923a2e16f2823c84bc79508cb132d9d0fd0a
LEE HYUN WOO <lhwda1388@naver.com>
2020-06-27 02:05:14 +0900

    간단한 테스트코드 추가

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { axe, toHaveNoViolations } from 'jest-axe'
+import renderer from 'react-test-renderer'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+expect.extend(toHaveNoViolations)
+
+describe('App component', () => {
+  test('renders text', () => {
+    /*
+    const { getByText } = render(<App />)
+    const linkElement = getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+    */
+
+    render(<App />)
+    expect(screen.getByText(/Sign up/)).toBeInTheDocument()
+  })
+
+  test('renders check router href link', () => {
+    render(<App />)
+    expect(screen.getByText('Sign up')).toHaveAttribute('href', '/signup')
+  })
+})
+
+describe('App accessibility Test', () => {
+  test('accessibility violations', async () => {
+    const { container } = render(<App />)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+  })
+  test('snapShot testing', () => {
+    const tree = renderer.create(<App />).toJSON()
+    console.log(tree)
+    expect(tree).toMatchSnapshot()
+  })
 })
